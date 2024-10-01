9320599c4dcc17f0a518cf55758126c734858f23
Camila2015 <167145122+Camila2015@users.noreply.github.com>
2024-09-30 22:32:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,38 +5,29 @@ from bokeh.models import CustomJS
 from streamlit_bokeh_events import streamlit_bokeh_events
 from PIL import Image
 import time
-import glob
 import paho.mqtt.client as paho
 import json
-from gtts import gTTS
-from googletrans import Translator
 
-def on_publish(client,userdata,result):             #create function for callback
+def on_publish(client, userdata, result):
     print("el dato ha sido publicado \n")
     pass
 
 def on_message(client, userdata, message):
     global message_received
     time.sleep(2)
-    message_received=str(message.payload.decode("utf-8"))
+    message_received = str(message.payload.decode("utf-8"))
     st.write(message_received)
 
-broker="broker.mqttdashboard.com"
-port=1883
-client1= paho.Client("GIT-HUB")
+broker = "broker.mqttdashboard.com"
+port = 1883
+client1 = paho.Client("GIT-HUB")
 client1.on_message = on_message
-
-
 
 st.title("Interfaces Multimodales")
 st.subheader("CONTROL POR VOZ")
 
 image = Image.open('voice_ctrl.jpg')
-
 st.image(image, width=200)
-
-
-
 
 st.write("Toca el Botón y habla ")
 
@@ -46,7 +37,7 @@ stt_button.js_on_event("button_click", CustomJS(code="""
     var recognition = new webkitSpeechRecognition();
     recognition.continuous = true;
     recognition.interimResults = true;
- 
+
     recognition.onresult = function (e) {
         var value = "";
         for (var i = e.resultIndex; i < e.results.length; ++i) {
@@ -54,7 +45,7 @@ stt_button.js_on_event("button_click", CustomJS(code="""
                 value += e.results[i][0].transcript;
             }
         }
-        if ( value != "") {
+        if (value != "") {
             document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
         }
     }
@@ -72,13 +63,15 @@ result = streamlit_bokeh_events(
 if result:
     if "GET_TEXT" in result:
         st.write(result.get("GET_TEXT"))
-        client1.on_publish = on_publish                            
-        client1.connect(broker,port)  
-        message =json.dumps({"Act1":result.get("GET_TEXT").strip()})
-        ret= client1.publish("voice_ctrl", message)
+        client1.on_publish = on_publish
+        client1.connect(broker, port)  
+        message = json.dumps({"Act1": result.get("GET_TEXT").strip()})
+        ret = client1.publish("voice_ctrl", message)
 
-    
-    try:
-        os.mkdir("temp")
-    except:
-        pass
+try:
+    os.mkdir("temp")
+except:
+    pass
+
+st.image("6oa.gif", use_column_width=True)  # Mostrar el GIF en la pantalla
+
